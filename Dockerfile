a7734bbbcaf55e66b4e02833c2390a48af0acf73
Said Nahúm <saidnahum@gmail.com>
2023-05-15 18:00:39 -0600

    First commit said

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:lts-alpine3.16
+
+RUN mkdir -p /home/app
+
+COPY . /home/app
+
+EXPOSE 3000
+
+CMD ["node", "/home/app/index.js"]
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
+
+const app = express()
+
+//mongoose.connect('mongodb://said:password@monguito:27017/miapp')
+//mongoose.connect('mongodb://said:password@monguito:27017/miapp?authSource=admin')
+//mongoose.connect('mongodb://said:password@monguito:localhost:27018/miapp')
+mongoose.connect('mongodb://said:password@monguito:27017/chanchitos?authSource=admin')
+
+app.get('/', async (_req, res) => {
+  console.log('listando... chanchitos...')
+  const animales = await Animal.find();
+  return res.send(animales)
+})
+app.get('/crear', async (_req, res) => {
+  console.log('creando...')
+  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
+  return res.send('ok')
+})
+
+app.listen(3000, () => console.log('listening...'))
